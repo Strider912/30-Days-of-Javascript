fcf00dc166bdedb0f97cbd233bf47a9ea6404014
Aman Bhardwaj <amanthecool912@gmail.com>
2026-01-07 19:45:04 +0530

    Added second implementation

--- Day1.js
+++ Day1.js
@@ -1,6 +1,8 @@
 /**
  * @return {Function}
  */
+
+// First Solution:
 var createHelloWorld = function() {
     
     return function(...args) {
@@ -12,3 +14,12 @@ var createHelloWorld = function() {
  * const f = createHelloWorld();
  * f(); // "Hello World"
  */
+
+/*
+Second Solution:
+
+var createHelloWorld = () => () => "Hello World";
+
+Time complexity: O(1)
+Space complexity: O(1)
+*/
