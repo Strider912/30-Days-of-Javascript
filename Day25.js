76cd5bb1588b91a5bc4e690480a2587ecdbfb491
Aman Bhardwaj <ambhardwaj@proofpoint.com>
2026-03-03 22:47:32 +0530

    formatted 25 file

--- Day25.js
+++ Day25.js
@@ -17,11 +17,10 @@ var flat = function (arr, n) {
       flattenedArr.push(arr[i]);
     }
   }
-  return flattenedArr
+  return flattenedArr;
 };
 
+const arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
 
-const arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
-
-const result = flat(arr, 5)
-console.log({result});
+const result = flat(arr, 5);
+console.log({ result });
